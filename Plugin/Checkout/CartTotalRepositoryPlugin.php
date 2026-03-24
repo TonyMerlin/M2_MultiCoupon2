6a1c8c61f5b27ca98d112b8078ff67fc240e8470
Tony Merlin <60714862+TonyMerlin@users.noreply.github.com>
2026-03-24 12:55:38 +0000

    Update CartTotalRepositoryPlugin.php

--- Plugin/Checkout/CartTotalRepositoryPlugin.php
+++ Plugin/Checkout/CartTotalRepositoryPlugin.php
@@ -3,27 +3,39 @@ declare(strict_types=1);
 
 namespace Merlin\MultiCoupon\Plugin\Checkout;
 
-use Magento\Quote\Api\CartRepositoryInterface;
 use Magento\Quote\Api\CartTotalRepositoryInterface;
-use Magento\Quote\Api\Data\TotalsExtensionFactory;
 use Magento\Quote\Api\Data\TotalsInterface;
+use Magento\Quote\Model\QuoteRepository;
 use Merlin\MultiCoupon\Model\QuoteCouponStorage;
 
 class CartTotalRepositoryPlugin
 {
+    /**
+     * @param QuoteRepository $quoteRepository
+     * @param QuoteCouponStorage $quoteCouponStorage
+     */
     public function __construct(
-        private readonly CartRepositoryInterface $cartRepository,
-        private readonly TotalsExtensionFactory $totalsExtensionFactory,
+        private readonly QuoteRepository $quoteRepository,
         private readonly QuoteCouponStorage $quoteCouponStorage
     ) {
     }
 
+    /**
+     * Add applied multi-coupon codes as extension data on the returned cart totals object.
+     *
+     * @param CartTotalRepositoryInterface $subject
+     * @param TotalsInterface $result
+     * @param int|string $cartId
+     * @return TotalsInterface
+     */
     public function afterGet(CartTotalRepositoryInterface $subject, TotalsInterface $result, $cartId): TotalsInterface
     {
-        $quote = $this->cartRepository->get((int)$cartId);
-        $extensionAttributes = $result->getExtensionAttributes() ?: $this->totalsExtensionFactory->create();
-        $extensionAttributes->setMerlinMultiCouponCodes($this->quoteCouponStorage->getCodes($quote));
-        $result->setExtensionAttributes($extensionAttributes);
+        $quote = $this->quoteRepository->getActive($cartId);
+        $extensionAttributes = $result->getExtensionAttributes();
+        if ($extensionAttributes) {
+            $extensionAttributes->setMerlinMultiCouponCodes($this->quoteCouponStorage->getCodes($quote));
+            $result->setExtensionAttributes($extensionAttributes);
+        }
 
         return $result;
     }
