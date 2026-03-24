7a90768e4d76e62f9a71de0aca6367f776ad06cd
Tony Merlin <60714862+TonyMerlin@users.noreply.github.com>
2026-03-24 12:55:17 +0000

    Update ClearNativeCouponObserver.php

--- Observer/ClearNativeCouponObserver.php
+++ Observer/ClearNativeCouponObserver.php
@@ -9,10 +9,19 @@ use Merlin\MultiCoupon\Model\QuoteCouponStorage;
 
 class ClearNativeCouponObserver implements ObserverInterface
 {
+    /**
+     * @param QuoteCouponStorage $quoteCouponStorage
+     */
     public function __construct(private readonly QuoteCouponStorage $quoteCouponStorage)
     {
     }
 
+    /**
+     * Clear Magento's native single coupon code when multi-coupon codes are present.
+     *
+     * @param Observer $observer
+     * @return void
+     */
     public function execute(Observer $observer): void
     {
         $quote = $observer->getEvent()->getQuote();
