66ff91181e2730518ef235d62389cb7583ae6299
Tony Merlin <60714862+TonyMerlin@users.noreply.github.com>
2026-03-24 12:49:18 +0000

    Update and rename Api/MultiCouponResponseInterface.php to Api/Data/MultiCouponResponseInterface.php

--- Api/Data/MultiCouponResponseInterface.php
+++ Api/Data/MultiCouponResponseInterface.php
@@ -9,19 +9,48 @@ interface MultiCouponResponseInterface
     public const MESSAGE = 'message';
     public const CODES = 'codes';
 
+    /**
+     * Return whether the operation succeeded.
+     *
+     * @return bool
+     */
     public function getSuccess(): bool;
+
+    /**
+     * Set whether the operation succeeded.
+     *
+     * @param bool $success
+     * @return self
+     */
     public function setSuccess(bool $success): self;
 
+    /**
+     * Return the operation message.
+     *
+     * @return string
+     */
     public function getMessage(): string;
+
+    /**
+     * Set the operation message.
+     *
+     * @param string $message
+     * @return self
+     */
     public function setMessage(string $message): self;
 
     /**
+     * Return the applied coupon codes.
+     *
      * @return string[]
      */
     public function getCodes(): array;
 
     /**
+     * Set the applied coupon codes.
+     *
      * @param string[] $codes
+     * @return self
      */
     public function setCodes(array $codes): self;
 }
