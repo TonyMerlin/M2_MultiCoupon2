c9a26e12066de82c1cdab3c46021ebdde197d306
Tony Merlin <60714862+TonyMerlin@users.noreply.github.com>
2026-03-24 12:54:15 +0000

    Update MultiCouponManagement.php

--- Model/MultiCouponManagement.php
+++ Model/MultiCouponManagement.php
@@ -3,54 +3,68 @@ declare(strict_types=1);
 
 namespace Merlin\MultiCoupon\Model;
 
-use Magento\Framework\Exception\LocalizedException;
-use Magento\Framework\Exception\NoSuchEntityException;
-use Magento\Quote\Api\CartRepositoryInterface;
-use Magento\Quote\Model\QuoteIdMaskFactory;
 use Magento\Checkout\Model\Session as CheckoutSession;
-use Magento\Customer\Model\Session as CustomerSession;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Quote\Api\CartRepositoryInterface;
 use Merlin\MultiCoupon\Api\Data\MultiCouponResponseInterface;
+use Merlin\MultiCoupon\Api\Data\MultiCouponResponseInterfaceFactory;
 use Merlin\MultiCoupon\Api\MultiCouponManagementInterface;
-use Merlin\MultiCoupon\Model\MultiCouponResponseFactory;
 
 class MultiCouponManagement implements MultiCouponManagementInterface
 {
+    /**
+     * @param CheckoutSession $checkoutSession
+     * @param CartRepositoryInterface $cartRepository
+     * @param QuoteCouponStorage $quoteCouponStorage
+     * @param Config $config
+     * @param MultiCouponResponseInterfaceFactory $responseFactory
+     */
     public function __construct(
         private readonly CheckoutSession $checkoutSession,
-        private readonly CustomerSession $customerSession,
         private readonly CartRepositoryInterface $cartRepository,
         private readonly QuoteCouponStorage $quoteCouponStorage,
-        private readonly RuleRepository $ruleRepository,
         private readonly Config $config,
-        private readonly MultiCouponResponseFactory $responseFactory
+        private readonly MultiCouponResponseInterfaceFactory $responseFactory
     ) {
     }
 
+    /**
+     * Return the current quote's applied multi-coupon codes.
+     *
+     * @return string[]
+     */
     public function getMyCodes(): array
     {
         return $this->quoteCouponStorage->getCodes($this->getActiveQuote());
     }
 
+    /**
+     * Add a coupon code to the active quote.
+     *
+     * @param string $code
+     * @return MultiCouponResponseInterface
+     */
     public function addForCustomer(string $code): MultiCouponResponseInterface
     {
         $code = $this->config->normalizeCode($code);
+        if ($code === '' || !$this->config->isAllowedCode($code)) {
+            return $this->buildResponse(false, 'Coupon code is not allowed.', $this->getMyCodes());
+        }
+
         $quote = $this->getActiveQuote();
-
-        if (!$this->config->isAllowedCode($code)) {
-            throw new LocalizedException(__('Coupon code %1 is not supported by Merlin Multi Coupon.', $code));
-        }
-
-        if (!$this->ruleRepository->getRuleByCode($quote, $code)) {
-            throw new LocalizedException(__('Coupon code %1 is not currently valid for this basket.', $code));
-        }
-
         $codes = $this->quoteCouponStorage->addCode($quote, $code);
         $quote->collectTotals();
         $this->cartRepository->save($quote);
 
-        return $this->buildResponse(true, (string)__('Coupon code %1 has been added.', $code), $codes);
+        return $this->buildResponse(true, 'Coupon code added.', $codes);
     }
 
+    /**
+     * Remove a coupon code from the active quote.
+     *
+     * @param string $code
+     * @return MultiCouponResponseInterface
+     */
     public function removeForCustomer(string $code): MultiCouponResponseInterface
     {
         $quote = $this->getActiveQuote();
@@ -58,9 +72,14 @@ class MultiCouponManagement implements MultiCouponManagementInterface
         $quote->collectTotals();
         $this->cartRepository->save($quote);
 
-        return $this->buildResponse(true, (string)__('Coupon code %1 has been removed.', $code), $codes);
+        return $this->buildResponse(true, 'Coupon code removed.', $codes);
     }
 
+    /**
+     * Clear all coupon codes from the active quote.
+     *
+     * @return MultiCouponResponseInterface
+     */
     public function clearForCustomer(): MultiCouponResponseInterface
     {
         $quote = $this->getActiveQuote();
@@ -68,24 +87,41 @@ class MultiCouponManagement implements MultiCouponManagementInterface
         $quote->collectTotals();
         $this->cartRepository->save($quote);
 
-        return $this->buildResponse(true, (string)__('All Merlin multi coupon codes have been removed.'), []);
+        return $this->buildResponse(true, 'Coupon codes cleared.', []);
     }
 
+    /**
+     * Return the active checkout quote.
+     *
+     * @return \Magento\Quote\Api\Data\CartInterface
+     * @throws LocalizedException
+     */
     private function getActiveQuote()
     {
         $quote = $this->checkoutSession->getQuote();
         if (!$quote || !$quote->getId()) {
-            throw new NoSuchEntityException(__('No active quote is available.'));
+            throw new LocalizedException(__('No active cart was found.'));
         }
+
         return $quote;
     }
 
+    /**
+     * Build a service response payload.
+     *
+     * @param bool $success
+     * @param string $message
+     * @param string[] $codes
+     * @return MultiCouponResponseInterface
+     */
     private function buildResponse(bool $success, string $message, array $codes): MultiCouponResponseInterface
     {
+        /** @var MultiCouponResponseInterface $response */
         $response = $this->responseFactory->create();
-        $response->setSuccess($success);
-        $response->setMessage($message);
-        $response->setCodes($codes);
+        $response->setSuccess($success)
+            ->setMessage($message)
+            ->setCodes($codes);
+
         return $response;
     }
 }
