327690ba7e7fe9a3a1d6fc80e27ae5b586b0ef8f
Tony Merlin <60714862+TonyMerlin@users.noreply.github.com>
2026-03-24 12:49:46 +0000

    Update MultiCouponManagementInterface.php

--- Api/MultiCouponManagementInterface.php
+++ Api/MultiCouponManagementInterface.php
@@ -7,11 +7,33 @@ use Merlin\MultiCoupon\Api\Data\MultiCouponResponseInterface;
 
 interface MultiCouponManagementInterface
 {
+    /**
+     * Return the currently applied multi-coupon codes for the active customer quote.
+     *
+     * @return string[]
+     */
     public function getMyCodes(): array;
 
+    /**
+     * Add a coupon code to the active customer quote.
+     *
+     * @param string $code
+     * @return MultiCouponResponseInterface
+     */
     public function addForCustomer(string $code): MultiCouponResponseInterface;
 
+    /**
+     * Remove a coupon code from the active customer quote.
+     *
+     * @param string $code
+     * @return MultiCouponResponseInterface
+     */
     public function removeForCustomer(string $code): MultiCouponResponseInterface;
 
+    /**
+     * Remove all multi-coupon codes from the active customer quote.
+     *
+     * @return MultiCouponResponseInterface
+     */
     public function clearForCustomer(): MultiCouponResponseInterface;
 }
