4384baa158e0da59d22b24738341d984a348ff4e
Tony Merlin <60714862+TonyMerlin@users.noreply.github.com>
2026-03-25 10:37:11 +0000

    Update AddCoupon.php

--- Controller/Cart/AddCoupon.php
+++ Controller/Cart/AddCoupon.php
@@ -7,8 +7,10 @@ use Magento\Checkout\Model\Session as CheckoutSession;
 use Magento\Framework\App\Action\Action;
 use Magento\Framework\App\Action\Context;
 use Magento\Framework\Controller\Result\Redirect;
-use Merlin\MultiCoupon\Model\QuoteCouponStorage;
 use Merlin\MultiCoupon\Model\Config;
+use Merlin\MultiCoupon\Model\Discount\ItemRuleMatcher;
+use Merlin\MultiCoupon\Model\QuoteCouponStorage;
+use Merlin\MultiCoupon\Model\RuleRepository;
 
 class AddCoupon extends Action
 {
@@ -17,12 +19,16 @@ class AddCoupon extends Action
      * @param CheckoutSession $checkoutSession
      * @param QuoteCouponStorage $quoteCouponStorage
      * @param Config $config
+     * @param RuleRepository $ruleRepository
+     * @param ItemRuleMatcher $itemRuleMatcher
      */
     public function __construct(
         Context $context,
         private readonly CheckoutSession $checkoutSession,
         private readonly QuoteCouponStorage $quoteCouponStorage,
-        private readonly Config $config
+        private readonly Config $config,
+        private readonly RuleRepository $ruleRepository,
+        private readonly ItemRuleMatcher $itemRuleMatcher
     ) {
         parent::__construct($context);
     }
@@ -35,7 +41,7 @@ class AddCoupon extends Action
     public function execute(): Redirect
     {
         $resultRedirect = $this->resultRedirectFactory->create();
-        $code = (string)$this->getRequest()->getParam('code');
+        $code = (string)($this->getRequest()->getParam('coupon_code') ?: $this->getRequest()->getParam('code'));
         $normalizedCode = $this->config->normalizeCode($code);
 
         if ($normalizedCode === '' || !$this->config->isAllowedCode($normalizedCode)) {
@@ -44,8 +50,39 @@ class AddCoupon extends Action
         }
 
         $quote = $this->checkoutSession->getQuote();
+
+        $existingCodes = $this->quoteCouponStorage->getCodes($quote);
+        if (in_array($normalizedCode, $existingCodes, true)) {
+            $this->messageManager->addNoticeMessage(__('Coupon %1 is already applied.', $normalizedCode));
+            return $resultRedirect->setPath('checkout/cart');
+        }
+
+        $rule = $this->ruleRepository->getRuleByCode($quote, $normalizedCode);
+        if (!$rule) {
+            $this->messageManager->addErrorMessage(__('Coupon code is not valid.'));
+            return $resultRedirect->setPath('checkout/cart');
+        }
+
+        $matchesAtLeastOneItem = false;
+        foreach ($quote->getAllVisibleItems() as $item) {
+            if ($this->itemRuleMatcher->isMatch($item, $rule)) {
+                $matchesAtLeastOneItem = true;
+                break;
+            }
+        }
+
+        if (!$matchesAtLeastOneItem) {
+            $this->messageManager->addErrorMessage(
+                __('Coupon code "%1" does not apply to any products currently in your basket.', $normalizedCode)
+            );
+            return $resultRedirect->setPath('checkout/cart');
+        }
+
         $this->quoteCouponStorage->addCode($quote, $normalizedCode);
-        $quote->collectTotals()->save();
+
+        $quote->setTotalsCollectedFlag(false);
+        $quote->collectTotals();
+        $quote->save();
 
         $this->messageManager->addSuccessMessage(__('Coupon %1 was added.', $normalizedCode));
         return $resultRedirect->setPath('checkout/cart');
