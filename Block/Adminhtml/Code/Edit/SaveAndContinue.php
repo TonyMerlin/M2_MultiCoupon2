6634bbcd83e478101e761e7c0fcce982a1d8f5cf
Tony Merlin <60714862+TonyMerlin@users.noreply.github.com>
2026-04-28 12:54:32 +0100

    Create SaveAndContinue.php

---
+++ Block/Adminhtml/Code/Edit/SaveAndContinue.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+
+namespace Merlin\MultiCoupon\Block\Adminhtml\Code\Edit;
+
+use Magento\Framework\View\Element\UiComponent\Control\ButtonProviderInterface;
+
+class SaveAndContinueButton extends GenericButton implements ButtonProviderInterface
+{
+    public function getButtonData(): array
+    {
+        return [
+            'label' => __('Save and Continue Edit'),
+            'class' => 'save',
+            'data_attribute' => [
+                'mage-init' => [
+                    'button' => [
+                        'event' => 'saveAndContinueEdit',
+                    ],
+                ],
+            ],
+            'sort_order' => 40,
+        ];
+    }
+}
